e6a0309e9851722ebdbf09fb96bfb2c3c2375fb9
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2024-10-29 03:44:11 +0000

    finished with 4° class

--- 03_openCV_techWithTim/02_exercises/04_drawing.py
+++ 03_openCV_techWithTim/02_exercises/04_drawing.py
@@ -35,28 +35,41 @@ def drawRectangle(frame, topLeft:Tuple[int, int], bottonRight:Tuple[int,int], co
     """
     return cv2.rectangle(frame, topLeft, bottonRight, color, thickness)
 
+def drawCirle(frame, center:Tuple[int,int], radius, color:Tuple[int, int, int], thickness):
+    """
+    the circle takes the center position and the radious
+    """
+    return cv2.circle(frame, center, radius, color, thickness)
+
+def writeText(frame, message:str, bottonLeft:Tuple[int, int]):
+    fontFamily = cv2.FONT_HERSHEY_SIMPLEX
+    fontSize = 1
+    fontColor = (255,0,255)
+    fontThickness = 2
+    fontScale = 1
+    return cv2.putText(frame, message, bottonLeft, fontFamily, fontSize, fontColor, fontThickness, cv2.LINE_AA)
+
 def display(frame):
         cv2.imshow("Camera", frame)
-
-
-    
+  
 capture = cv2.VideoCapture(0)
+width = getWidth(capture)
+height = getHeight(capture)
+print(f"width:{width}\nheight{height}")
 
 while True:
     ret, frame = capture.read()
-    width = getWidth(capture)
-    height = getHeight(capture)
     """
     Since those drawings are inside a loop, it gives the impression the shapes
     are being drawn at the same time, but this is not really the case.
     """
     display(drawLine(frame,(0,0), (width,height), (0,0,255), 5))
     display(drawLine(frame,(0,height), (width,0), (255,0,0), 5))
-    display(drawRectangle(frame,(0,500), (250,750), (0,255,0), 5))
-
+    display(drawRectangle(frame,(0,240), (250,400), (0,255,0), -1)) #-1 to fill the shape
+    display(drawCirle(frame, (300, 300), 100, (100,100,0), 5))
+    display(writeText(frame,"hello!", (200,150)))
     if cv2.waitKey(1) == ord('q'):
         break
 
 cv2.release()
-cv2.destroyAllWindows() 
-    
+cv2.destroyAllWindows()
